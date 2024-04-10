985118d3d3959a887d64a1b921ef14d27e4932ae
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-04-09 21:11:57 -0300

    Lista de tareas v1

---
+++ Proyecto To-Do List/js/main.js
@@ -0,0 +1,11 @@
+const motor = document.getElementById('motor');
+const boton = document.getElementById('btn');
+const listador = document.querySelector('ul');
+const vacio = document.getElementById('void')
+
+
+boton.addEventListener('click', function(e){
+        e.preventDefault();
+  
+    
+})
