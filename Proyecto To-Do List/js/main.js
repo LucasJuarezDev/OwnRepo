7e42dda071066a153ca9e998924d93ba6c659996
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-04-13 15:41:59 -0300

    Update main.js

--- Proyecto To-Do List/js/main.js
+++ Proyecto To-Do List/js/main.js
@@ -4,8 +4,31 @@ const listador = document.querySelector('ul');
 const vacio = document.getElementById('void')
 
 
-boton.addEventListener('click', function(e){
+boton.addEventListener('click', (e) =>{
         e.preventDefault();
-  
-    
+        
+        
+        const texto = motor.value;
+        
+        if(texto != ""){
+                const list = document.createElement("input");
+                
+                
+                listador.appendChild(list);
+                listador.appendChild(btn_delete(list));
+                
+        }
 })
+
+function btn_delete(list){ //list es pasado como parametro, entonces el btn_delete puede manipularlo a su antojo
+        const borrar = document.createElement("button");
+        borrar.textContent = "x";
+        borrar.setAttribute("id", "btn"); //herencia de id al elemento de 'borrar'
+
+        borrar.addEventListener('click', (e) =>{
+                listador.removeChild(list);      
+                listador.removeChild(borrar);
+        })
+
+        return borrar;
+}
