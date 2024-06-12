ecc8e5623df1f0b9270817caec7d962e3a78d2db
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-06-12 17:48:09 -0300

    UPDATE 12/06

--- PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/VentaManager.h
+++ PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/VentaManager.h
@@ -1,5 +1,23 @@
 #ifndef VENTAMANAGER_H_INCLUDED
 #define VENTAMANAGER_H_INCLUDED
+#include "VentaArchivo.h"
+#include "PrendaManager.h"
+#include "Venta.h"
+#include "PrendaArchivo.h"
+#include "Prenda.h"
+#include "Fecha.h"
+
+class VentaManager{
+private:
+    VentaArchivo _ArchVenta;
+    PrendaManager _PrendManager;
+    PrendaArchivo _ArchiPrenda;
+    Prenda _Prenda;
+public:
+    void SubMenuCargarVenta();
+
+
+};
 
 
 
