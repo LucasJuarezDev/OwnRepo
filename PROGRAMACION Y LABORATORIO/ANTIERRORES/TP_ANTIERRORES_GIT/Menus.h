c2edeaec82fe0d562a97db16e89c24153c4c8830
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-06-12 17:45:09 -0300

    UPDATE 12/06

--- PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/Menus.h
+++ PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/Menus.h
@@ -1,30 +1,53 @@
 #ifndef MENUS_H_INCLUDED
 #define MENUS_H_INCLUDED
-#include "PrendaManager.h"
-#include "VentaManager.h"
+#include <windows.h>
 #include <iostream>
 using namespace std;
+#include "PrendaManager.h"
+#include "VentaManager.h"
+
+void SetColorSubMenu(int color) {
+    HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+    SetConsoleTextAttribute(hConsole, color);
+}
 
 //      MENU DE PRENDA
 
 PrendaManager obj_de_submenu_prenda;
+VentaManager obj_de_submenu_venta;
 
 void Menu1()
 
 {
+    SetColorSubMenu(5);
      int opcion;
     while (true){
         system("cls");
-        cout << "PRENDAS" << endl;
-        cout << "----------------" << endl;
-        cout << "1) CARGAR PRENDA" << endl;
-        cout << "2) MODIFICAR PRENDA" << endl;
-        cout << "3) ELIMINAR PRENDA" << endl;
-        cout << "4) CARGAR VENTA" << endl;
-        cout << "---------------------------" << endl;
-        cout << "0) SALIR" << endl;
+        const int menuWidth = 30;  // Ancho del menú
+        const int consoleWidth = 90;  // Ancho de la consola
+        int padding = (consoleWidth - menuWidth) / 2;
+        string pad(padding, ' ');
+
+        string horizontalLine(menuWidth + 2, '-');  // +2 para las barras verticales
+
+        cout << pad << "+" << horizontalLine << "+" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(21) << "PRENDAS" << setw(12) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "1)" << setw(19) << "CARGAR PRENDA" << setw(9) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "2)" << setw(21) << "MODIFICAR PRENDA" << setw(7) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "3)" << setw(20) << "ELIMINAR PRENDA" << setw(8) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "4)" << setw(19) << "CARGAR VENTA" << setw(9) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "0)" << setw(16) << "SALIR" << setw(12) << " |" << endl;
+        cout << pad << "+" << horizontalLine << "+" << endl;
 
         cin >> opcion;
+        system("cls");
 
         switch(opcion){
             case 1:
@@ -45,16 +68,18 @@ void Menu1()
                 break;
             case 4:
                 {
-                    //CARGAR VENTA
+                    obj_de_submenu_venta.SubMenuCargarVenta();
                 }
                 break;
             case 0:
                 {
+                    SetColorSubMenu(10);
                     return;
                 }
                 break;
             default:
                 cout << "OPCION INCORRECTA" << endl;
+                system("pause");
             break;
         }
 
@@ -65,17 +90,31 @@ void Menu1()
 
 void Menu2()
 {
+    SetColorSubMenu(12);
     int opcion;
     while (true){
         system("cls");
-        cout << "INVENTARIO" << endl;
-        cout << "----------------" << endl;
-        cout << "1) VER INVENTARIO" << endl;
-        cout << "2) DETALLES DE PRENDA" << endl;
-        cout << "---------------------------" << endl;
-        cout << "0) SALIR" << endl;
+        const int menuWidth = 30;  // Ancho del menú
+        const int consoleWidth = 90;  // Ancho de la consola
+        int padding = (consoleWidth - menuWidth) / 2;
+        string pad(padding, ' ');
+
+        string horizontalLine(menuWidth + 2, '-');  // +2 para las barras verticales
+
+        cout << pad << "+" << horizontalLine << "+" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(23) << "INVENTARIO" << setw(10) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "1)" << setw(19) << "VER INVENTARIO" << setw(9) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "2)" << setw(21) << "DETALLES DE PRENDA" << setw(7) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(5) << "0)" << setw(15) << "SALIR" << setw(13) << " |" << endl;
+        cout << pad << "+" << horizontalLine << "+" << endl;
 
         cin >> opcion;
+        system("cls");
 
         switch(opcion){
             case 1:
@@ -91,11 +130,13 @@ void Menu2()
                 break;
             case 0:
                 {
+                    SetColorSubMenu(10);
                     return;
                 }
                 break;
             default:
                 cout << "OPCION INCORRECTA" << endl;
+                system("pause");
             break;
         }
 
@@ -106,18 +147,34 @@ void Menu2()
 
 void Menu3()
 {
+    SetColorSubMenu(9);
     int opcion;
     while (true){
         system("cls");
-        cout << "ESTADISTICA" << endl;
-        cout << "----------------" << endl;
-        cout << "1) ESTADISTICAS DE PRODUCTOS" << endl;
-        cout << "2) HISTORIAL DE VENTA" << endl;
-        cout << "3) PORCENTAJE DE GANANCIAS CON RESPECTO A LAS VENTAS DEL A�O " << endl;
-        cout << "---------------------------" << endl;
-        cout << "0) SALIR" << endl;
+        const int menuWidth = 30;  // Ancho del menú
+        const int consoleWidth = 90;  // Ancho de la consola
+        int padding = (consoleWidth - menuWidth) / 2;
+        string pad(padding, ' ');
+
+        string horizontalLine(menuWidth + 2, '-');  // +2 para las barras verticales
+
+        cout << pad << "+" << horizontalLine << "+" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(21) << "ESTADISTICAS" << setw(12) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(2) << "1)" << setw(28) << "ESTADISTICAS DEL PRODUCTO" << setw(3) << "|" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(2) << "2)" << setw(24) << "HISTORIAL DE VENTAS" << setw(7) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(2) << "3)" << setw(23) << "PORC. DE GANANCIAS" << setw(8) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(32) << "(C/ RESPECTO AL ANIO ANTERIOR)" << "|" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(33) << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << "--------------------------" << " |" << endl;
+        cout << pad << "|" << setw(menuWidth + 1) << right << setw(2) << "0)" << setw(19) << "SALIR" << setw(12) << " |" << endl;
+        cout << pad << "+" << horizontalLine << "+" << endl;
+
 
         cin >> opcion;
+        system("cls");
 
         switch(opcion){
             case 1:
@@ -137,11 +194,13 @@ void Menu3()
                 break;
             case 0:
                 {
+                    SetColorSubMenu(10);
                     return;
                 }
                 break;
             default:
                 cout << "OPCION INCORRECTA" << endl;
+                system("pause");
             break;
         }
 
