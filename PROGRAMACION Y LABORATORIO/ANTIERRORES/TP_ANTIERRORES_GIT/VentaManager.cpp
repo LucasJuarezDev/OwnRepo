e66204d8a29cf4246a6c1a5757f1bdf71dfc6f02
JUAREZ.DEV <147560945+LucasJuarezDev@users.noreply.github.com>
2024-06-12 17:47:49 -0300

    UPDATE 12/06

--- PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/VentaManager.cpp
+++ PROGRAMACION Y LABORATORIO/ANTIERRORES/TP_ANTIERRORES_GIT/VentaManager.cpp
@@ -1,1 +1,52 @@
 #include "VentaManager.h"
+
+void VentaManager::SubMenuCargarVenta(){
+    Venta _ven;
+    Fecha obj;
+
+    cout << "BIENVENIDO! ESTA USTED POR REALIZAR UNA VENTA" << endl;
+    cout << "----------------------------------------------" << endl;
+    _PrendManager.SubmenuInventario();
+
+    bool bandera=true;
+
+    while(bandera!=false){
+        int CodigoSeleccionado;
+        cout<<"seleccione el codigo de la prenda que desea vender"<<endl;
+        cin>>CodigoSeleccionado;
+
+        //entramos al archivo de prenda y rrecorremos todas las prendas
+        //filtramos por el codigo elegido
+        //seteamos el objeto que creamos
+        //lo subimos al archivi de venta
+
+       int CantPrendas= _ArchiPrenda.ContarRegistrosPrenda();
+
+       for(int x=0;x<CantPrendas;x++){
+            _Prenda=_ArchiPrenda.LeerPrenda(x);
+
+            if(_Prenda.getCodigo()==CodigoSeleccionado){
+                _ven.setCodigoPrenda(_Prenda.getCodigo());
+                _ven.setNombrePrenda(_Prenda.getNombrePrenda());
+                _ven.setModelo(_Prenda.getModelo());
+                _ven.setTalle(_Prenda.getTalle());
+                _ven.setColor(_Prenda.getColor());
+                _ven.setCantidad(1);
+                _ven.setColor(_Prenda.getColor());
+                _ven.setVentaFecha(obj.ReturnFechaActual());
+
+                //SI QUEREMOS MOSTRAR EL OBJETO FECHA:    "_ven.getVentaFecha().MostrarFechaActual();"
+
+                //guarda en archivo
+            }
+
+       }
+
+        int continuar;
+        cout<<"desea agregar otro producto a la venta? 1-si 2-no"<<endl;
+        cin>>continuar;
+
+        if(continuar==2){bandera=false;}
+    }
+
+}
